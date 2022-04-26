9c7cfca6fd7ca41e90bd6e57af71433bd7d0e38a
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-04-26 12:50:12 -0700

    halfway-there

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,47 @@
+import random
+
+
 def play_game():
-    pass
+    # computer reads file
+    with open('test-word.txt') as file_contents:
+        # reads the contents
+        contents_string = file_contents.read()
+        contents_list = contents_string.split()
+        random_word = random.choice(contents_list)
+        print(random_word)
+        print(f'I am thinking of a word. It has {len(random_word)}letters...')
+
+        guesses = ' '
+        turns = 8
+
+        while turns > 0:
+            failed = 0
+
+            for letter in random_word:
+                if letter in guesses:
+                    print(letter, end=" ")
+
+                else:
+                    print("_")
+                    print(letter, end=" ")
+                    failed += 1
+
+            if failed == 0:
+                print("You Win")  
+                print("The word is: ", random_word) 
+                break   
+
+            print()
+            guess = input("Guess a letter:")
+            guesses += guess
+
+            if guess not in random_word:
+                turns -= 1
+                print("Wrong")
+                print("You have", + turns, 'more guesses')
+
+                if turns == 0:
+                    print("You Lose")
 
 
 if __name__ == "__main__":
