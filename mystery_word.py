b8e0d360fb49939d961c88ab44d80d8a558923ce
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-04-27 12:32:52 -0700

    added-difficulty-levels

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,32 @@
 import random
 
 
+def get_list(): 
+    EASY_WORDS = []
+    MEDIUM_WORDS = []
+    DIFFICULT_WORDS = []
+
+    with open('words.txt') as file_contents:
+        # reads the contents
+        contents_string = file_contents.read()
+        contents_list = contents_string.split()
+
+    for word in contents_list:
+        if 4 <= len(word) <= 6:
+            EASY_WORDS.append(word)
+        elif 7 <= len(word) <= 8:
+            MEDIUM_WORDS.append(word)
+        elif 9 <= len(word):
+            DIFFICULT_WORDS.append(word)
+    level = input("Choose your difficuly level: E, M, D : ").upper()   
+    if level == "E": 
+        return EASY_WORDS
+    elif level == "M":
+        return MEDIUM_WORDS
+    elif level == "D":
+        return DIFFICULT_WORDS    
+
+
 def play_again():
     print("Game over. Would you like to play again? y/n")
     play_response = input()
@@ -18,45 +44,42 @@ def play_again():
 
 def play_game():
     # computer reads file
-    with open('words.txt') as file_contents:
-        # reads the contents
-        contents_string = file_contents.read()
-        contents_list = contents_string.split()
-        random_word = random.choice(contents_list)
-        # print(random_word)
-        print(f'I am thinking of a word. It has {len(random_word)} letters...')
+    contents_list = get_list()
+    random_word = random.choice(contents_list)
+    # print(random_word)
+    print(f'I am thinking of a word. It has {len(random_word)} letters...')
 
-        guesses = ' '
-        turns = 8
+    guesses = ' '
+    turns = 8
 
-        while turns > 0:
-            failed = 0
+    while turns > 0:
+        failed = 0
 
-            for letter in random_word:
-                if letter in guesses:
-                    print(letter, end=" ")
+        for letter in random_word:
+            if letter in guesses:
+                print(letter, end=" ")
 
-                else:
-                    print("_", end=" ")
-                    failed += 1
+            else:
+                print("_", end=" ")
+                failed += 1
 
-            if failed == 0:
-                print("The word is: ", random_word)
-                print("You Win")
+        if failed == 0:
+            print("The word is: ", random_word)
+            print("You Win")
+            play_again()
+
+        print()
+        guess = input("Guess a letter:")
+        guesses += guess
+
+        if guess not in random_word:
+            turns -= 1
+            print("Wrong")
+            print("You have", + turns, 'more guesses')
+
+            if turns == 0:
+                print("You Lose, the word is: ", random_word)
                 play_again()
-
-            print()
-            guess = input("Guess a letter:")
-            guesses += guess
-
-            if guess not in random_word:
-                turns -= 1
-                print("Wrong")
-                print("You have", + turns, 'more guesses')
-
-                if turns == 0:
-                    print("You Lose, the word is: ", random_word)
-                    play_again()
 
 
 if __name__ == "__main__":
